1e3eba6935ab1f82f50ce5b9921b3ae5c702a75d
Lawrence Simon <lawsimon3105@gmail.com>
2023-10-15 21:11:48 +0200

    Added the basic code to get the total of Incomes and Expenses

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,18 @@
+const totalIncomeElem = document.getElementById("total-income");
+const totalOutcomeElem = document.getElementById("total-outcome");
 
+function calculateTotalOfTransactions(transactionArray) {
+    let transactionTotal = 0;
+    for (let index = 0; index < transactionArray.length; index++) {
+        console.log("working");
+        transactionTotal += Number(transactionArray[index][2]);
+    }
+    return transactionTotal;
+}
+
+if (expenseArray.length > 0) {
+    totalOutcomeElem.innerHTML = calculateTotalOfTransactions(expenseArray);
+}
+if (incomeArray.length > 0) {
+    totalIncomeElem.innerHTML = calculateTotalOfTransactions(incomeArray);
+}
