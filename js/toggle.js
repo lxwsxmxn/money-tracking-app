036878b4d86ef779971eae76dbac775acd43736b
Lawrence Simon <lawsimon3105@gmail.com>
2023-10-19 08:44:49 +0200

    Completed

--- js/toggle.js
+++ js/toggle.js
@@ -1,6 +1,16 @@
 function toggleList(btn) {
     // Toggles between the expense and income lists
+
+    // Select list containers
+    const expenseContainer = document.getElementById("expense-container");
+    const incomeContainer = document.getElementById("income-container");
+    
     if (btn.id == "expense-toggle-button") {
+        expenseContainer.classList.remove("hide");
+        incomeContainer.classList.add("hide");
     }
     if (btn.id == "income-toggle-button") {
+        incomeContainer.classList.remove("hide");
+        expenseContainer.classList.add("hide");
     }
+}
