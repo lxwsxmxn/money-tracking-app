944e9566b80c71c14bca31cb3594c5a8b839424a
Lawrence Simon <lawsimon3105@gmail.com>
2023-10-19 08:19:55 +0200

    Adding toggle for the expense and income lists

---
+++ js/toggle.js
@@ -0,0 +1,6 @@
+function toggleList(btn) {
+    // Toggles between the expense and income lists
+    if (btn.id == "expense-toggle-button") {
+    }
+    if (btn.id == "income-toggle-button") {
+    }
